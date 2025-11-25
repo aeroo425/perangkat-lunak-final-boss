7d6a6d27236b937ce84ca99138a90d34bae9e861
Desyka <email@widya.com>
2025-11-25 18:55:07 +0700

    user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,40 +2,37 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> 8fdd84c8f2916a1539198c1935ba56070fa9f0b2
-=======
-use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> e7a250d36fffaa619ed974660d4f9300cf6c1f73
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'email',
-        'phone',
-        'profile_photo',
         'password',
         'otp_code', // <-- TAMBAHKAN INI
         'otp_expires_at', // <-- TAMBAHKAN INI
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> e7a250d36fffaa619ed974660d4f9300cf6c1f73
 
     /**
      * The attributes that should be cast.
@@ -46,8 +43,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'otp_expires_at' => 'datetime', // <-- TAMBAHKAN INI
     ];
-<<<<<<< HEAD
->>>>>>> 8fdd84c8f2916a1539198c1935ba56070fa9f0b2
-=======
->>>>>>> e7a250d36fffaa619ed974660d4f9300cf6c1f73
 }
